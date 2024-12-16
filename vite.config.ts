c3d2a2b36f17f11a79c197443cf63922b507cb24
imaraafricasafaris <imaraafricasafaris@gmail.com>
2024-12-16 06:12:33 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/public_html/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
@@ -19,5 +20,7 @@ export default defineConfig({
     assetsInlineLimit: 4096,
     sourcemap: false,
     minify: 'terser',
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
